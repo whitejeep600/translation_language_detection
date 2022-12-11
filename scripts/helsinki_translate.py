b520c194ebc26591d2e45064f6319de66f72c143
Antoni Krzysztof Maciag <antoni.maciag1@gmail.com>
2022-12-11 12:02:39 +0800

    debugging helsinki

--- scripts/helsinki_translate.py
+++ scripts/helsinki_translate.py
@@ -5,17 +5,21 @@ from transformers import pipeline
 
 if __name__ == '__main__':
     model_checkpoint = "Helsinki-NLP/opus-mt-id-en"
-    translator = pipeline("translation", model=model_checkpoint)
+    translator = pipeline("translation", model=model_checkpoint, device=0)
     paragraphs = []
     with open('data/original/indonesian/for_monolingual.jsonl') as f:
         for line in f:
             paragraphs.append(json.loads(line))
 
     train_paragraphs = []
+    skipped_train = 0
     progress = tqdm(total=1600)
     for i in range(1600, 3200):
-        train_paragraphs.append({'paragraph': translator(paragraphs[i]['translation']['in'], max_length=1024)[0]['translation_text'],
-                                 'language': 'indonesian'})
+        if len(translator.tokenizer.tokenize(paragraphs[i]['translation']['in'])) < 512:
+            train_paragraphs.append({'paragraph': translator(paragraphs[i]['translation']['in'], max_length=512)[0]['translation_text'],
+                                     'language': 'indonesian'})
+        else:
+            skipped_train += 1
         progress.update(1)
     train_json = json.dumps(train_paragraphs, indent=4)
     with open("data/translated/from_indonesian/train_helsinki.json", "w") as file:
@@ -24,7 +28,7 @@ if __name__ == '__main__':
     test_paragraphs = []
     progress = tqdm(total=350)
     for i in range(3200, 3550):
-    	test_paragraphs.append({'paragraph': translator(paragraphs[i]['translation']['in'], max_length=1024)[0]['translation_text'],
+        test_paragraphs.append({'paragraph': translator(paragraphs[i]['translation']['in'], max_length=512)[0]['translation_text'],
                                 'language': 'indonesian'})
         progress.update(1)
     test_json = json.dumps(test_paragraphs, indent=4)
