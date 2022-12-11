5ce4d53d8a5b8c75eb3bf64cb67e57cc6ae61e43
Antoni Krzysztof Maciag <antoni.maciag1@gmail.com>
2022-12-11 11:23:12 +0800

    changes to helsinki

--- scripts/helsinki_translate.py
+++ scripts/helsinki_translate.py
@@ -13,20 +13,20 @@ if __name__ == '__main__':
 
     train_paragraphs = []
     progress = tqdm(total=1600)
-    for i in range(1600):
-        train_paragraphs.append({'paragraph': translator(paragraphs[i])[0]['translation_text'],
+    for i in range(1600, 3200):
+        train_paragraphs.append({'paragraph': translator(paragraphs[i]['translation']['in'], max_length=1024)[0]['translation_text'],
                                  'language': 'indonesian'})
         progress.update(1)
     train_json = json.dumps(train_paragraphs, indent=4)
-    with open("../data/translated/from_indonesian/train_helsinki.json", "w") as file:
+    with open("data/translated/from_indonesian/train_helsinki.json", "w") as file:
         file.write(train_json)
 
     test_paragraphs = []
     progress = tqdm(total=350)
-    for i in range(1600, 1950):
-        test_paragraphs.append({'paragraph': translator(paragraphs[i])[0]['translation_text'],
+    for i in range(3200, 3550):
+    	test_paragraphs.append({'paragraph': translator(paragraphs[i]['translation']['in'], max_length=1024)[0]['translation_text'],
                                 'language': 'indonesian'})
         progress.update(1)
     test_json = json.dumps(test_paragraphs, indent=4)
-    with open("../data/translated/from_indonesian/test_helsinki.json", "w") as file:
+    with open("data/translated/from_indonesian/test_helsinki.json", "w") as file:
         file.write(test_json)
