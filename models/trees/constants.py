0616f98c909586dd9ae010ed38001b7b7ad30d1a
antoni.maciag1@gmail.com <antoni.maciag1@gmail.com>
2022-12-16 15:42:20 +0800

    added pycache to gitignore

--- models/trees/constants.py
+++ models/trees/constants.py
@@ -4,3 +4,24 @@ label_to_int = {
     'indonesian': 2,
     'japanese': 3
 }
+
+CONVOLUTION_LENGTH = 8
+
+pos_to_int = {
+    'ADJ': 0,
+    'ADP': 1,
+    'ADV': 2,
+    'AUX': 3,
+    'CCONJ': 4,
+    'DET': 5,
+    'INTJ': 6,
+    'NOUN': 7,
+    'NUM': 8,
+    'PART': 9,
+    'PRON': 10,
+    'PROPN': 11,
+    'PUNCT': 12,
+    'SCONJ': 13,
+    'SYM': 14,
+    'VERB': 15,
+}
--- models/trees/dependency parsing.py
+++ models/trees/dependency parsing.py
@@ -1,11 +1,33 @@
+import random
+
 import stanza
 
+from translation_language_detection.models.trees.train import read_training_data
 
-def parse_sentence(sentence):
-    pipeline = stanza.Pipeline('en', processors='tokenize,mwt,pos,lemma,depparse')
+
+def length_to_root(tree, index):
+    if tree.sentences[0].tokens[index].words[0].head == 0:
+        return 0
+    else:
+        return length_to_root(tree, tree.sentences[0].tokens[index].words[0].head - 1) + 1
+
+
+def tree_height(tree):
+    return max([length_to_root(tree, index) for index in range(len(tree.sentences[0].tokens))])
+
+
+def parse_sentence(sentence, pipeline):
     tree = pipeline(sentence)
     return tree
 
 
 if __name__ == '__main__':
-    print(parse_sentence("your father jumps over your lazy mother"))
+    pipeline = stanza.Pipeline('en', processors='tokenize,mwt,pos,lemma,depparse')
+    # all_sentences = read_training_data()
+    # random.shuffle(all_sentences)
+    # sample = [sentence['text'] for sentence in all_sentences[:128]]
+    # lengths = [tree_height(tree) for tree in [parse_sentence(sentence, pipeline) for sentence in sample]]
+    # print(sorted(lengths))
+    # print(sum(lengths)/len(lengths))
+    # about 10% of height greater than 8
+    print(parse_sentence("your father jumps over your very lazy mother", pipeline))
